16772fa2e03cdc6f76ff8beeac63df5130df4d4d
Jordi Armadans <jordiarmadans@125.Xarxa-10-83-71.eduroam.upc.edu>
2016-01-27 10:05:14 +0100

    quart commit

--- FirstApp/www/js/controllers.js
+++ FirstApp/www/js/controllers.js
@@ -1,12 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-    $scope.busca = function(cercador) {
+.controller('DashCtrl', function($scope, Ciutats) {
+    $scope.ciutats = Ciutats.all();
+    $scope.buscar = function(cercador) {
       alert(cercador);
     }
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Ciutats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,14 +16,14 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+  $scope.chats = Ciutats.all();
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+    Ciutats.remove(chat);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams, Ciutats) {
+  $scope.chat = Ciutats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
